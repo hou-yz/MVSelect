8b538c6083efc8b52a3b8a8f5885ec98cbcc8fca
Zhangzeyu97 <503423253@qq.com>
2022-10-13 13:27:58 +1100

    Update trainer.py

--- multiview_detector/trainer.py
+++ multiview_detector/trainer.py
@@ -202,81 +202,79 @@ class ModelNetTrainer_12(object):
         self.model_name = model_name
         self.log_dir = log_dir
         self.num_views = num_views
+        self.best_acc = 0
+        self.i_acc = 0
 
         self.model.cuda()
         if self.log_dir is not None:
             self.writer = SummaryWriter(log_dir)
 
 
-    def train(self, n_epochs):
-
-        best_acc = 0
-        i_acc = 0
+    def train(self, epoch):
         self.model.train()
-        for epoch in range(n_epochs):
-            # permute data for mvcnn
-            rand_idx = np.random.permutation(int(len(self.train_loader.dataset.filepaths)/self.num_views))
-            filepaths_new = []
-            for i in range(len(rand_idx)):
-                filepaths_new.extend(self.train_loader.dataset.filepaths[rand_idx[i]*self.num_views:(rand_idx[i]+1)*self.num_views])
-            self.train_loader.dataset.filepaths = filepaths_new
+        # permute data for mvcnn
+        rand_idx = np.random.permutation(int(len(self.train_loader.dataset.filepaths)/self.num_views))
+        filepaths_new = []
+        for i in range(len(rand_idx)):
+            filepaths_new.extend(self.train_loader.dataset.filepaths[rand_idx[i]*self.num_views:(rand_idx[i]+1)*self.num_views])
+        self.train_loader.dataset.filepaths = filepaths_new
 
-            # plot learning rate
-            lr = self.optimizer.state_dict()['param_groups'][0]['lr']
-            self.writer.add_scalar('params/lr', lr, epoch)
+        # plot learning rate
+        lr = self.optimizer.state_dict()['param_groups'][0]['lr']
+        self.writer.add_scalar('params/lr', lr, epoch)
 
-            # train one epoch
-            out_data = None
-            in_data = None
-            for i, data in enumerate(self.train_loader):
+        # train one epoch
+        out_data = None
+        in_data = None
+        for i, data in enumerate(self.train_loader):
 
-                if self.model_name == 'mvcnn':
-                    N,V,C,H,W = data[1].size()
-                    in_data = Variable(data[1]).view(-1,C,H,W).cuda()
-                else:
-                    in_data = Variable(data[1].cuda())
-                target = Variable(data[0]).cuda().long()
+            if self.model_name == 'mvcnn':
+                N,V,C,H,W = data[1].size()
+                in_data = Variable(data[1]).view(-1,C,H,W).cuda()
+            else:
+                in_data = Variable(data[1].cuda())
+            target = Variable(data[0]).cuda().long()
 
-                self.optimizer.zero_grad()
+            self.optimizer.zero_grad()
 
-                out_data = self.model(in_data)
+            out_data = self.model(in_data)
 
-                loss = self.loss_fn(out_data, target)
-                
-                self.writer.add_scalar('train/train_loss', loss, i_acc+i+1)
+            loss = self.loss_fn(out_data, target)
+            
+            self.writer.add_scalar('train/train_loss', loss, self.i_acc+i+1)
 
-                pred = torch.max(out_data, 1)[1]
-                results = pred == target
-                correct_points = torch.sum(results.long())
+            pred = torch.max(out_data, 1)[1]
+            results = pred == target
+            correct_points = torch.sum(results.long())
 
-                acc = correct_points.float()/results.size()[0]
-                self.writer.add_scalar('train/train_overall_acc', acc, i_acc+i+1)
+            acc = correct_points.float()/results.size()[0]
+            self.writer.add_scalar('train/train_overall_acc', acc, self.i_acc+i+1)
 
-                loss.backward()
-                self.optimizer.step()
-                
-                log_str = 'epoch %d, step %d: train_loss %.3f; train_acc %.3f' % (epoch+1, i+1, loss, acc)
-                if (i+1)%100==0:
-                    print(log_str)
-            i_acc += i
+            loss.backward()
+            self.optimizer.step()
+            
+            log_str = 'epoch %d, step %d: train_loss %.3f; train_acc %.3f' % (epoch+1, i+1, loss, acc)
+            if (i+1)%100==0:
+                print(log_str)
+        self.i_acc += i
 
-            # evaluation
-            if (epoch+1)%1==0:
-                with torch.no_grad():
-                    loss, val_overall_acc, val_mean_class_acc = self.update_validation_accuracy(epoch)
-                self.writer.add_scalar('val/val_mean_class_acc', val_mean_class_acc, epoch+1)
-                self.writer.add_scalar('val/val_overall_acc', val_overall_acc, epoch+1)
-                self.writer.add_scalar('val/val_loss', loss, epoch+1)
+        # evaluation
+        if (epoch+1)%1==0:
+            with torch.no_grad():
+                loss, val_overall_acc, val_mean_class_acc = self.update_validation_accuracy(epoch)
+            self.writer.add_scalar('val/val_mean_class_acc', val_mean_class_acc, epoch+1)
+            self.writer.add_scalar('val/val_overall_acc', val_overall_acc, epoch+1)
+            self.writer.add_scalar('val/val_loss', loss, epoch+1)
 
-            # save best model
-            if val_overall_acc > best_acc:
-                best_acc = val_overall_acc
-                self.model.save(self.log_dir, epoch)
- 
-            # adjust learning rate manually
-            if epoch > 0 and (epoch+1) % 10 == 0:
-                for param_group in self.optimizer.param_groups:
-                    param_group['lr'] = param_group['lr']*0.5
+        # save best model
+        if val_overall_acc > self.best_acc:
+            self.best_acc = val_overall_acc
+            self.model.save(self.log_dir, epoch)
+
+        # adjust learning rate manually
+        if epoch > 0 and (epoch+1) % 10 == 0:
+            for param_group in self.optimizer.param_groups:
+                param_group['lr'] = param_group['lr']*0.5
 
         # export scalar data to JSON for external processing
         self.writer.export_scalars_to_json(self.log_dir+"/all_scalars.json")
@@ -341,84 +339,83 @@ class ModelNetTrainer_20(object):
         self.model_name = model_name
         self.log_dir = log_dir
         self.num_views = num_views
+        self.best_acc = 0
+        self.i_acc = 0
         self.model.cuda()
         if self.log_dir is not None:
             self.writer = SummaryWriter(log_dir)
-    def train(self, n_epochs):
-        best_acc = 0
-        i_acc = 0
+    def train(self, epoch):
         self.model.train()
-        for epoch in range(n_epochs):
-            if self.model_name == 'view_gcn':
-                if epoch == 1:
-                    for param_group in self.optimizer.param_groups:
-                        param_group['lr'] = lr
-                if epoch > 1:
-                    for param_group in self.optimizer.param_groups:
-                        param_group['lr'] = param_group['lr'] * 0.5 * ( 1 + math.cos(epoch * math.pi / 15))
+        if self.model_name == 'view_gcn':
+            if epoch == 1:
+                for param_group in self.optimizer.param_groups:
+                    param_group['lr'] = lr
+            if epoch > 1:
+                for param_group in self.optimizer.param_groups:
+                    param_group['lr'] = param_group['lr'] * 0.5 * ( 1 + math.cos(epoch * math.pi / 15))
+        else:
+            if epoch > 0 and (epoch + 1) % 10 == 0:
+                for param_group in self.optimizer.param_groups:
+                    param_group['lr'] = param_group['lr'] * 0.5
+        # permute data for mvcnn
+        rand_idx = np.random.permutation(int(len(self.train_loader.dataset.filepaths) / self.num_views))
+        filepaths_new = []
+        for i in range(len(rand_idx)):
+            filepaths_new.extend(self.train_loader.dataset.filepaths[
+                                    rand_idx[i] * self.num_views:(rand_idx[i] + 1) * self.num_views])
+        self.train_loader.dataset.filepaths = filepaths_new
+        # plot learning rate
+        lr = self.optimizer.state_dict()['param_groups'][0]['lr']
+        self.writer.add_scalar('params/lr', lr, epoch)
+        # train one epoch
+        out_data = None
+        in_data = None
+        for i, data in enumerate(self.train_loader):
+            if self.model_name == 'view-gcn' and epoch == 0:
+                for param_group in self.optimizer.param_groups:
+                    param_group['lr'] = lr * ((i + 1) / (len(rand_idx) // 20))
+            if self.model_name == 'view-gcn':
+                N, V, C, H, W = data[1].size()
+                in_data = Variable(data[1]).view(-1, C, H, W).cuda()
             else:
-                if epoch > 0 and (epoch + 1) % 10 == 0:
-                    for param_group in self.optimizer.param_groups:
-                        param_group['lr'] = param_group['lr'] * 0.5
-            # permute data for mvcnn
-            rand_idx = np.random.permutation(int(len(self.train_loader.dataset.filepaths) / self.num_views))
-            filepaths_new = []
-            for i in range(len(rand_idx)):
-                filepaths_new.extend(self.train_loader.dataset.filepaths[
-                                     rand_idx[i] * self.num_views:(rand_idx[i] + 1) * self.num_views])
-            self.train_loader.dataset.filepaths = filepaths_new
-            # plot learning rate
-            lr = self.optimizer.state_dict()['param_groups'][0]['lr']
-            self.writer.add_scalar('params/lr', lr, epoch)
-            # train one epoch
-            out_data = None
-            in_data = None
-            for i, data in enumerate(self.train_loader):
-                if self.model_name == 'view-gcn' and epoch == 0:
-                    for param_group in self.optimizer.param_groups:
-                        param_group['lr'] = lr * ((i + 1) / (len(rand_idx) // 20))
-                if self.model_name == 'view-gcn':
-                    N, V, C, H, W = data[1].size()
-                    in_data = Variable(data[1]).view(-1, C, H, W).cuda()
-                else:
-                    in_data = Variable(data[1].cuda())
-                target = Variable(data[0]).cuda().long()
-                target_ = target.unsqueeze(1).repeat(1, 4*(10+5)).view(-1)
-                self.optimizer.zero_grad()
-                if self.model_name == 'view-gcn':
-                    out_data, F_score,F_score2= self.model(in_data)
-                    out_data_ = torch.cat((F_score, F_score2), 1).view(-1, 40)
-                    loss = self.loss_fn(out_data, target)+ self.loss_fn(out_data_, target_)
-                else:
-                    out_data = self.model(in_data)
-                    loss = self.loss_fn(out_data, target)
-                self.writer.add_scalar('train/train_loss', loss, i_acc + i + 1)
+                in_data = Variable(data[1].cuda())
+            target = Variable(data[0]).cuda().long()
+            target_ = target.unsqueeze(1).repeat(1, 4*(10+5)).view(-1)
+            self.optimizer.zero_grad()
+            if self.model_name == 'view-gcn':
+                out_data, F_score,F_score2= self.model(in_data)
+                out_data_ = torch.cat((F_score, F_score2), 1).view(-1, 40)
+                loss = self.loss_fn(out_data, target)+ self.loss_fn(out_data_, target_)
+            else:
+                out_data = self.model(in_data)
+                loss = self.loss_fn(out_data, target)
+            self.writer.add_scalar('train/train_loss', loss, self.i_acc + i + 1)
 
-                pred = torch.max(out_data, 1)[1]
-                results = pred == target
-                correct_points = torch.sum(results.long())
+            pred = torch.max(out_data, 1)[1]
+            results = pred == target
+            correct_points = torch.sum(results.long())
 
-                acc = correct_points.float() / results.size()[0]
-                self.writer.add_scalar('train/train_overall_acc', acc, i_acc + i + 1)
-                #print('lr = ', str(param_group['lr']))
-                loss.backward()
-                self.optimizer.step()
-                log_str = 'epoch %d, step %d: train_loss %.3f; train_acc %.3f' % (epoch + 1, i + 1, loss, acc)
-                if (i + 1) % 100 == 0:
-                    print(log_str)
-            i_acc += i
-            # evaluation
-            if (epoch + 1) % 1 == 0:
-                with torch.no_grad():
-                    loss, val_overall_acc, val_mean_class_acc = self.update_validation_accuracy(epoch)
-                self.writer.add_scalar('val/val_mean_class_acc', val_mean_class_acc, epoch + 1)
-                self.writer.add_scalar('val/val_overall_acc', val_overall_acc, epoch + 1)
-                self.writer.add_scalar('val/val_loss', loss, epoch + 1)
-                self.model.save(self.log_dir, epoch)
-            # save best model
-                if val_overall_acc > best_acc:
-                    best_acc = val_overall_acc
-                print('best_acc', best_acc)
+            acc = correct_points.float() / results.size()[0]
+            self.writer.add_scalar('train/train_overall_acc', acc, self.i_acc + i + 1)
+            #print('lr = ', str(param_group['lr']))
+            loss.backward()
+            self.optimizer.step()
+            log_str = 'epoch %d, step %d: train_loss %.3f; train_acc %.3f' % (epoch + 1, i + 1, loss, acc)
+            if (i + 1) % 100 == 0:
+                print(log_str)
+        self.i_acc += i
+        # evaluation
+        if (epoch + 1) % 1 == 0:
+            with torch.no_grad():
+                loss, val_overall_acc, val_mean_class_acc = self.update_validation_accuracy(epoch)
+            self.writer.add_scalar('val/val_mean_class_acc', val_mean_class_acc, epoch + 1)
+            self.writer.add_scalar('val/val_overall_acc', val_overall_acc, epoch + 1)
+            self.writer.add_scalar('val/val_loss', loss, epoch + 1)
+            self.model.save(self.log_dir, epoch)
+        # save best model
+            if val_overall_acc > self.best_acc:
+                self.best_acc = val_overall_acc
+            print('best_acc', self.best_acc)
         # export scalar data to JSON for external processing
         self.writer.export_scalars_to_json(self.log_dir + "/all_scalars.json")
         self.writer.close()
