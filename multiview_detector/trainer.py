dbd941777209efe79dbea781111b551806ae310a
Zhangzeyu97 <503423253@qq.com>
2022-10-13 13:00:02 +1100

    Update trainer.py

--- multiview_detector/trainer.py
+++ multiview_detector/trainer.py
@@ -13,7 +13,9 @@ from multiview_detector.utils.decode import ctdet_decode, mvdet_decode
 from multiview_detector.utils.nms import nms
 from multiview_detector.utils.meters import AverageMeter
 from multiview_detector.utils.image_utils import add_heatmap_to_image, img_color_denormalize
-
+from torch.autograd import Variable
+from tensorboardX import SummaryWriter
+import math
 
 class BaseTrainer(object):
     def __init__(self):
@@ -184,3 +186,288 @@ class PerspectiveTrainer(BaseTrainer):
         # print(f'Test, loss: {losses / len(dataloader):.6f}, Time: {t_epoch:.3f}')
 
         return losses / len(dataloader), moda
+    
+    
+
+class ModelNetTrainer_12(object):
+
+    def __init__(self, model, train_loader, val_loader, optimizer, \
+                 model_name, log_dir, num_views=12):
+
+        self.optimizer = optimizer
+        self.model = model
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.loss_fn = nn.CrossEntropyLoss() # loss_fn
+        self.model_name = model_name
+        self.log_dir = log_dir
+        self.num_views = num_views
+
+        self.model.cuda()
+        if self.log_dir is not None:
+            self.writer = SummaryWriter(log_dir)
+
+
+    def train(self, n_epochs):
+
+        best_acc = 0
+        i_acc = 0
+        self.model.train()
+        for epoch in range(n_epochs):
+            # permute data for mvcnn
+            rand_idx = np.random.permutation(int(len(self.train_loader.dataset.filepaths)/self.num_views))
+            filepaths_new = []
+            for i in range(len(rand_idx)):
+                filepaths_new.extend(self.train_loader.dataset.filepaths[rand_idx[i]*self.num_views:(rand_idx[i]+1)*self.num_views])
+            self.train_loader.dataset.filepaths = filepaths_new
+
+            # plot learning rate
+            lr = self.optimizer.state_dict()['param_groups'][0]['lr']
+            self.writer.add_scalar('params/lr', lr, epoch)
+
+            # train one epoch
+            out_data = None
+            in_data = None
+            for i, data in enumerate(self.train_loader):
+
+                if self.model_name == 'mvcnn':
+                    N,V,C,H,W = data[1].size()
+                    in_data = Variable(data[1]).view(-1,C,H,W).cuda()
+                else:
+                    in_data = Variable(data[1].cuda())
+                target = Variable(data[0]).cuda().long()
+
+                self.optimizer.zero_grad()
+
+                out_data = self.model(in_data)
+
+                loss = self.loss_fn(out_data, target)
+                
+                self.writer.add_scalar('train/train_loss', loss, i_acc+i+1)
+
+                pred = torch.max(out_data, 1)[1]
+                results = pred == target
+                correct_points = torch.sum(results.long())
+
+                acc = correct_points.float()/results.size()[0]
+                self.writer.add_scalar('train/train_overall_acc', acc, i_acc+i+1)
+
+                loss.backward()
+                self.optimizer.step()
+                
+                log_str = 'epoch %d, step %d: train_loss %.3f; train_acc %.3f' % (epoch+1, i+1, loss, acc)
+                if (i+1)%100==0:
+                    print(log_str)
+            i_acc += i
+
+            # evaluation
+            if (epoch+1)%1==0:
+                with torch.no_grad():
+                    loss, val_overall_acc, val_mean_class_acc = self.update_validation_accuracy(epoch)
+                self.writer.add_scalar('val/val_mean_class_acc', val_mean_class_acc, epoch+1)
+                self.writer.add_scalar('val/val_overall_acc', val_overall_acc, epoch+1)
+                self.writer.add_scalar('val/val_loss', loss, epoch+1)
+
+            # save best model
+            if val_overall_acc > best_acc:
+                best_acc = val_overall_acc
+                self.model.save(self.log_dir, epoch)
+ 
+            # adjust learning rate manually
+            if epoch > 0 and (epoch+1) % 10 == 0:
+                for param_group in self.optimizer.param_groups:
+                    param_group['lr'] = param_group['lr']*0.5
+
+        # export scalar data to JSON for external processing
+        self.writer.export_scalars_to_json(self.log_dir+"/all_scalars.json")
+        self.writer.close()
+
+    def update_validation_accuracy(self, epoch):
+        all_correct_points = 0
+        all_points = 0
+        count = 0
+        wrong_class = np.zeros(40)
+        samples_class = np.zeros(40)
+        all_loss = 0
+        self.model.eval()
+
+        for _, data in enumerate(self.val_loader, 0):
+
+            if self.model_name == 'mvcnn':
+                N,V,C,H,W = data[1].size()
+                in_data = Variable(data[1]).view(-1,C,H,W).cuda()
+            else:#'svcnn'
+                in_data = Variable(data[1]).cuda()
+            target = Variable(data[0]).cuda()
+
+            out_data = self.model(in_data)
+            pred = torch.max(out_data, 1)[1]
+            all_loss += self.loss_fn(out_data, target).cpu().data.numpy()
+            results = pred == target
+
+            for i in range(results.size()[0]):
+                if not bool(results[i].cpu().data.numpy()):
+                    wrong_class[target.cpu().data.numpy().astype('int')[i]] += 1
+                samples_class[target.cpu().data.numpy().astype('int')[i]] += 1
+            correct_points = torch.sum(results.long())
+
+            all_correct_points += correct_points
+            all_points += results.size()[0]
+
+        print('Total # of test models: ', all_points)
+        class_acc = (samples_class - wrong_class) / samples_class
+        val_mean_class_acc = np.mean(class_acc)
+        acc = all_correct_points.float() / all_points
+        val_overall_acc = acc.cpu().data.numpy()
+        loss = all_loss / len(self.val_loader)
+
+        print('val mean class acc. : ', val_mean_class_acc)
+        print('val overall acc. : ', val_overall_acc)
+        print('val loss : ', loss)
+        print(class_acc)
+        self.model.train()
+
+        return loss, val_overall_acc, val_mean_class_acc
+
+
+class ModelNetTrainer_20(object):
+    def __init__(self, model, train_loader, val_loader, optimizer, \
+                 model_name, log_dir, num_views=12):
+        self.optimizer = optimizer
+        self.model = model
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.loss_fn = nn.CrossEntropyLoss()
+        self.model_name = model_name
+        self.log_dir = log_dir
+        self.num_views = num_views
+        self.model.cuda()
+        if self.log_dir is not None:
+            self.writer = SummaryWriter(log_dir)
+    def train(self, n_epochs):
+        best_acc = 0
+        i_acc = 0
+        self.model.train()
+        for epoch in range(n_epochs):
+            if self.model_name == 'view_gcn':
+                if epoch == 1:
+                    for param_group in self.optimizer.param_groups:
+                        param_group['lr'] = lr
+                if epoch > 1:
+                    for param_group in self.optimizer.param_groups:
+                        param_group['lr'] = param_group['lr'] * 0.5 * ( 1 + math.cos(epoch * math.pi / 15))
+            else:
+                if epoch > 0 and (epoch + 1) % 10 == 0:
+                    for param_group in self.optimizer.param_groups:
+                        param_group['lr'] = param_group['lr'] * 0.5
+            # permute data for mvcnn
+            rand_idx = np.random.permutation(int(len(self.train_loader.dataset.filepaths) / self.num_views))
+            filepaths_new = []
+            for i in range(len(rand_idx)):
+                filepaths_new.extend(self.train_loader.dataset.filepaths[
+                                     rand_idx[i] * self.num_views:(rand_idx[i] + 1) * self.num_views])
+            self.train_loader.dataset.filepaths = filepaths_new
+            # plot learning rate
+            lr = self.optimizer.state_dict()['param_groups'][0]['lr']
+            self.writer.add_scalar('params/lr', lr, epoch)
+            # train one epoch
+            out_data = None
+            in_data = None
+            for i, data in enumerate(self.train_loader):
+                if self.model_name == 'view-gcn' and epoch == 0:
+                    for param_group in self.optimizer.param_groups:
+                        param_group['lr'] = lr * ((i + 1) / (len(rand_idx) // 20))
+                if self.model_name == 'view-gcn':
+                    N, V, C, H, W = data[1].size()
+                    in_data = Variable(data[1]).view(-1, C, H, W).cuda()
+                else:
+                    in_data = Variable(data[1].cuda())
+                target = Variable(data[0]).cuda().long()
+                target_ = target.unsqueeze(1).repeat(1, 4*(10+5)).view(-1)
+                self.optimizer.zero_grad()
+                if self.model_name == 'view-gcn':
+                    out_data, F_score,F_score2= self.model(in_data)
+                    out_data_ = torch.cat((F_score, F_score2), 1).view(-1, 40)
+                    loss = self.loss_fn(out_data, target)+ self.loss_fn(out_data_, target_)
+                else:
+                    out_data = self.model(in_data)
+                    loss = self.loss_fn(out_data, target)
+                self.writer.add_scalar('train/train_loss', loss, i_acc + i + 1)
+
+                pred = torch.max(out_data, 1)[1]
+                results = pred == target
+                correct_points = torch.sum(results.long())
+
+                acc = correct_points.float() / results.size()[0]
+                self.writer.add_scalar('train/train_overall_acc', acc, i_acc + i + 1)
+                #print('lr = ', str(param_group['lr']))
+                loss.backward()
+                self.optimizer.step()
+                log_str = 'epoch %d, step %d: train_loss %.3f; train_acc %.3f' % (epoch + 1, i + 1, loss, acc)
+                if (i + 1) % 100 == 0:
+                    print(log_str)
+            i_acc += i
+            # evaluation
+            if (epoch + 1) % 1 == 0:
+                with torch.no_grad():
+                    loss, val_overall_acc, val_mean_class_acc = self.update_validation_accuracy(epoch)
+                self.writer.add_scalar('val/val_mean_class_acc', val_mean_class_acc, epoch + 1)
+                self.writer.add_scalar('val/val_overall_acc', val_overall_acc, epoch + 1)
+                self.writer.add_scalar('val/val_loss', loss, epoch + 1)
+                self.model.save(self.log_dir, epoch)
+            # save best model
+                if val_overall_acc > best_acc:
+                    best_acc = val_overall_acc
+                print('best_acc', best_acc)
+        # export scalar data to JSON for external processing
+        self.writer.export_scalars_to_json(self.log_dir + "/all_scalars.json")
+        self.writer.close()
+
+    def update_validation_accuracy(self, epoch):
+        all_correct_points = 0
+        all_points = 0
+        count = 0
+        wrong_class = np.zeros(40)
+        samples_class = np.zeros(40)
+        all_loss = 0
+        self.model.eval()
+
+        for _, data in enumerate(self.val_loader, 0):
+
+            if self.model_name == 'view-gcn':
+                N, V, C, H, W = data[1].size()
+                in_data = Variable(data[1]).view(-1, C, H, W).cuda()
+            else:  # 'svcnn'
+                in_data = Variable(data[1]).cuda()
+            target = Variable(data[0]).cuda()
+            if self.model_name == 'view-gcn':
+                out_data,F1,F2=self.model(in_data)
+            else:
+                out_data = self.model(in_data)
+            pred = torch.max(out_data, 1)[1]
+            all_loss += self.loss_fn(out_data, target).cpu().data.numpy()
+            results = pred == target
+
+            for i in range(results.size()[0]):
+                if not bool(results[i].cpu().data.numpy()):
+                    wrong_class[target.cpu().data.numpy().astype('int')[i]] += 1
+                samples_class[target.cpu().data.numpy().astype('int')[i]] += 1
+            correct_points = torch.sum(results.long())
+
+            all_correct_points += correct_points
+            all_points += results.size()[0]
+
+        print('Total # of test models: ', all_points)
+        class_acc = (samples_class - wrong_class) / samples_class
+        val_mean_class_acc = np.mean(class_acc)
+        acc = all_correct_points.float() / all_points
+        val_overall_acc = acc.cpu().data.numpy()
+        loss = all_loss / len(self.val_loader)
+
+        print('val mean class acc. : ', val_mean_class_acc)
+        print('val overall acc. : ', val_overall_acc)
+        print('val loss : ', loss)
+        print(class_acc)
+        self.model.train()
+
+        return loss, val_overall_acc, val_mean_class_acc
