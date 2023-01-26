0ca31f5abe91560a350d261733503cf1755c9c04
Yunzhong Hou <hou_yz@outlook.com>
2023-01-26 21:49:10 +1100

    Update draw_curve.py

--- src/utils/draw_curve.py
+++ src/utils/draw_curve.py
@@ -12,7 +12,7 @@ def draw_curve(path, x_epoch, train_loss, test_loss, train_result=None, test_res
     ax1.legend()
     if train_result is not None:
         ax2 = fig.add_subplot(122, title="acc")
-        ax2.plot(x_epoch, train_result, 'bo-', label='train' + ': {:.1f}'.format(test_result[-1]))
+        ax2.plot(x_epoch, train_result, 'bo-', label='train' + ': {:.1f}'.format(train_result[-1]))
     else:
         ax2 = fig.add_subplot(122, title="moda")
     ax2.plot(x_epoch, test_result, 'ro-', label='test' + ': {:.1f}'.format(test_result[-1]))
