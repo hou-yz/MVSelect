644aec75bfa4b9a1a1cbbe46b8e60abf7e4e4ab4
Yunzhong Hou <hou_yz@outlook.com>
2023-02-20 11:12:19 +1100

    Update imgDataset.py

--- src/datasets/imgDataset.py
+++ src/datasets/imgDataset.py
@@ -16,11 +16,10 @@ class imgDataset(VisionDataset):
                   'person', 'piano', 'plant', 'radio', 'range_hood', 'sink', 'sofa', 'stairs',
                   'stool', 'table', 'tent', 'toilet', 'tv_stand', 'vase', 'wardrobe', 'xbox']
 
-    def __init__(self, root, num_cam, mode='multi', split='train', per_cls_instances=0, dropout=0.0):
+    def __init__(self, root, num_cam, split='train', per_cls_instances=0, dropout=0.0):
         super().__init__(root)
         self.num_cam, self.num_class = num_cam, len(self.classnames)
-        assert mode in ['single', 'multi']
-        self.mode = mode
+        self.split = split
         self.transform = T.Compose([T.Resize([224, 224]), T.ToTensor(),
                                     T.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)), ])
         self.dropout = dropout
@@ -41,41 +40,30 @@ class imgDataset(VisionDataset):
         print(f'{split}: {self.num_class} classes, {num_cam} views, {len(self.targets)} instances')
 
     def __len__(self):
-        return len(self.targets) if self.mode == 'multi' else len(self.targets) * self.num_cam
+        return len(self.targets)
 
     def __getitem__(self, idx, visualize=False):
-        if self.mode == 'multi':
-            imgs = []
-            for cam in range(self.num_cam):
-                img = Image.open(self.img_fpaths[cam][idx]).convert('RGB')
-                if visualize:
-                    plt.imshow(img)
-                    plt.show()
-                imgs.append(self.transform(img))
-            imgs = torch.stack(imgs)
-            tgt = self.targets[idx]
-            # dropout
-            drop, keep_cams = np.random.rand() < self.dropout, torch.ones(self.num_cam, dtype=torch.bool)
-            if drop:
-                num_drop = np.random.randint(self.num_cam - 1)
-                drop_cams = np.random.choice(self.num_cam, num_drop, replace=False)
-                for cam in drop_cams:
-                    keep_cams[cam] = 0
-        elif self.mode == 'single':
-            img = Image.open(self.img_fpaths[idx % self.num_cam][idx // self.num_cam]).convert('RGB')
+        imgs = []
+        for cam in range(self.num_cam):
+            img = Image.open(self.img_fpaths[cam][idx]).convert('RGB')
             if visualize:
                 plt.imshow(img)
                 plt.show()
-            imgs = self.transform(img)
-            tgt = self.targets[idx // self.num_cam]
-            keep_cams = 1
-        else:
-            raise Exception
+            imgs.append(self.transform(img))
+        imgs = torch.stack(imgs)
+        tgt = self.targets[idx]
+        # dropout
+        drop, keep_cams = np.random.rand() < self.dropout, torch.ones(self.num_cam, dtype=torch.bool)
+        if drop:
+            num_drop = np.random.randint(self.num_cam - 1)
+            drop_cams = np.random.choice(self.num_cam, num_drop, replace=False)
+            for cam in drop_cams:
+                keep_cams[cam] = 0
 
         return imgs, tgt, keep_cams
 
 
 if __name__ == '__main__':
-    dataset = imgDataset('/home/houyz/Data/modelnet/modelnet40_images_new_12x', 12, mode='multi')
+    dataset = imgDataset('/home/houyz/Data/modelnet/modelnet40_images_new_12x', 12)
     dataset.__getitem__(0)
     dataset.__getitem__(len(dataset) - 1, visualize=True)
