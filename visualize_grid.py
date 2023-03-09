d1b5b5e0d56e1f04df9132d0ede4fabeaa58642f
Yunzhong Hou <hou_yz@outlook.com>
2023-03-09 12:58:31 +1100

    visualization

--- visualize_grid.py
+++ visualize_grid.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     precs = np.loadtxt(
-        '/home/houyz/Code/MVselect/logs/wildtrack/resnet18_max_lr0.0005_b2_e10_dropcam0.0_2022-11-09_19-53-14/moda_90.7_Lstrategy65.1_Rstrategy67.6_theory70.8_avg61.0.txt')
+        '/home/houyz/Code/MVselect/logs/modelnet40_12/resnet18_max_down1_lr5e-05_b8_e10_dropcam0.0_2023-02-26_04-03-42/prec_94.0_Lstrategy85.8_Rstrategy85.5_theory86.0_avg69.1.txt')
     precs = precs[len(precs) // 2:]
     N = len(precs)
 
@@ -50,15 +50,19 @@ if __name__ == '__main__':
     # prob[10, 0] = 1
     # prob[11, 9] = 1
 
-
     prob = np.zeros([N, N])
-    prob[0, 1] = 1
-    prob[1, 0] = 1
-    prob[2, 0] = 1
-    prob[3, 2] = 1
-    prob[4, 0] = 1
-    prob[5, 1] = 1
-    prob[6, 0] = 1
+    prob[0, [1, 4, 7, 10]] = [0.59, 0.13, 0.01, 0.26]
+    prob[1, [4, 7, 9, 10]] = [0.19, 0.04, 0.01, 0.74]
+    prob[2, [1, 4, 7, 9, 10]] = [0.51, 0.11, 0.01, 0.01, 0.36]
+    prob[3, [1, 4, 7, 10]] = [0.64, 0.1, 0.03, 0.22]
+    prob[4, [1, 3, 7, 9, 10]] = [0.64, 0.01, 0.12, 0.03, 0.22]
+    prob[5, [1, 3, 7, 10]] = [0.53, 0.10, 0.03, 0.36]
+    prob[6, [1, 4, 7, 10]] = [0.58, 0.12, 0.01, 0.29]
+    prob[7, [1, 4, 9, 10]] = [0.49, 0.12, 0.01, 0.32]
+    prob[8, [1, 4, 7, 9, 10]] = [0.53, 0.10, 0.01, 0.01, 0.35]
+    prob[9, [1, 4, 7, 10]] = [0.62, 0.11, 0.04, 0.23]
+    prob[10, [1, 3, 4, 7, 9, 11]] = [0.69, 0.01, 0.18, 0.10, 0.01, 0.01]
+    prob[11, [1, 4, 7, 10]] = [0.53, 0.11, 0.01, 0.35]
 
     plt.figure(figsize=(5, 5))
     plt.imshow(prob, cmap='Blues')
--- visualize_img.py
+++ visualize_img.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import torch
 import os
 from src.datasets import frameDataset, MultiviewX, Wildtrack, imgDataset
@@ -19,20 +19,24 @@ def set_border(img, width=5, fill=(0, 255, 0)):
 
 if __name__ == '__main__':
     denorm = img_color_denormalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-    dataset = frameDataset(Wildtrack(os.path.expanduser('~/Data/Wildtrack')), split='test', )
-    imgs, world_gt, imgs_gt, affine_mats, frame, keep_cams = dataset[0]
+    # dataset = frameDataset(Wildtrack(os.path.expanduser('~/Data/Wildtrack')), split='test', )
+    # imgs, world_gt, imgs_gt, affine_mats, frame, keep_cams = dataset[0]
 
     # dataset = imgDataset(os.path.expanduser('~/Data/modelnet/modelnet40v1png'), 12, split='test')
-    # # dataset = imgDataset(os.path.expanduser('~/Data/modelnet/modelnet40v2png_ori4'), 20, split='test')
-    # imgs, tgt, keep_cams = dataset[0]
-    imgs = denorm(imgs)
-    # imgs[1] = set_border(imgs[1])  # , fill=(255, 192, 0)
-    # imgs[10] = set_border(imgs[10])  # , fill=(0, 176, 80)
-    imgs[0] = set_border(imgs[0], width=20)  # , fill=(255, 192, 0)
-    imgs[2] = set_border(imgs[2], width=20)  # , fill=(0, 176, 80)
+    dataset = imgDataset(os.path.expanduser('~/Data/modelnet/modelnet40v2png_ori4'), 20, split='test')
+    for i in range(30):
+        # index=np.random.randint(len(dataset))
+        index = i
+        imgs, tgt, keep_cams = dataset[index+100]
+        imgs = denorm(imgs)
+        # imgs[0] = set_border(imgs[0])  # , fill=(255, 192, 0)
+        # imgs[9] = set_border(imgs[9])  # , fill=(0, 176, 80)
+        # imgs[0] = set_border(imgs[0], width=20)  # , fill=(255, 192, 0)
+        # imgs[1] = set_border(imgs[1], width=20)  # , fill=(0, 176, 80)
+        # imgs[5] = set_border(imgs[5], width=20)  # , fill=(0, 176, 80)
 
-    imgs_grid = make_grid(imgs, nrow=4)
-    save_image(imgs_grid, 'imgs_grid.png')
-    plt.imshow(imgs_grid.permute([1, 2, 0]))
-    plt.show()
+        imgs_grid = make_grid(imgs, nrow=5)
+        # save_image(imgs_grid, 'imgs_grid.png')
+        plt.imshow(imgs_grid.permute([1, 2, 0]))
+        plt.show()
     pass
